38ab102bda35b09aefab5d3614442f3e3dd92ce1
Olena Kameneva <lenakr0910@gmail.com>
2022-12-06 01:23:57 +0300

    save111

--- src/index.js
+++ src/index.js
@@ -1,67 +1,114 @@
+import ImagesApiService from './service_image';
+import LoadMoreBtn from './load-more';
+// import '../sass/index.scss';
 import Notiflix from 'notiflix';
-import ImageQuery from './service_image';
-// import SimpleLightbox from "simplelightbox";
-// import "simplelightbox/dist/simple-lightbox.min.css";
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-
-const searchForm = document.querySelector('#search-form');
-const imageGallery = document.querySelector('.gallery');
-const loadMoreBtn = document.querySelector('.load');
-
-const newImage = new ImageQuery();
-
-searchForm.addEventListener('submit', onSearch);
-loadMoreBtn.addEventListener('click', onload);
-
-
- async function onSearch(evt) {
-    evt.preventDefaul();
-
-     newQuery.query = evt.currentTarget.elements.searchQuery.value;
-     
-
-     await newImage.fetchImage().then(data => console.log(data));
-     
-    if (!imageQuery.query) {
-        Notiflix.Notify.failure('Empty query. Please input something for search.' )
-        return;
-     }
-      newImage.resetPage();
-     clearGallery()
-}
-
-function onload() {
-    newImage.fetchImage().then(data => console.log(data));
-}
-
-function createMarkup(arr) {
-     const markupImg = arr.map(({ webformatURL,
-        largeImageURL,
-        tags,
-        likes,
-        views,
-        comments,
-        downloads }) => `<div class="photo-card">
-    <a href="${largeImageURL}" class="gallery__item">
-    < img class="img" src="${webformatURL}" alt = ${tags} loading = "lazy" />
-        </a>
-  <div class="info">
-    <p class="info-item"><b>Likes</b>${likes}</p>
-    <p class="info-item"><b>Views</b>${views}</p>
-    <p class="info-item"><b>Comments</b>${comments}</p>
-    <p class="info-item"><b>Downloads</b>${downloads}</p>
-    </div>
-</div>`).join();
-    
-    imageGallery.insertAdjacentHTML('beforeend', markupImg);
+const refs = {
+    form: document.querySelector('.search-form'),
+    galleryContainer: document.querySelector('.gallery'),
 };
 
-function clearGallery() {
-    imageGallery.innerHTML = '';
+let hitsLength = 40;
+
+const loadMoreBtn = new LoadMoreBtn({
+    selector: '.load-more',
+    hidden: true,
+});
+
+const imagesApiService = new ImagesApiService();
+
+refs.form.addEventListener('submit', onSearch);
+loadMoreBtn.refs.button.addEventListener('click', fetchImages);
+
+function onSearch(e) {
+    e.preventDefault();
+
+    imagesApiService.query = e.currentTarget.elements.searchQuery.value;
+
+    if (!imagesApiService.query) {
+        Notiflix.Notify.failure(
+            'Search box cannot be empty. Please enter the word.'
+        );
+        return;
+    }
+
+    loadMoreBtn.show();
+    imagesApiService.resetPage();
+    clearGalleryContainer();
+
+    fetchImages();
+
+    refs.form.reset();
 }
 
+function fetchImages() {
+    loadMoreBtn.disable();
+    imagesApiService.fetchImages().then(({ totalHits, hits }) => {
+        if (hitsLength > totalHits) {
+            loadMoreBtn.hide();
+            Notiflix.Notify.failure(
+                "We're sorry, but you've reached the end of search results."
+            );
+            return;
+        } else if (hits.length === 0) {
+            Notiflix.Notify.failure(
+                'Sorry, there are no images matching your search query. Please try again.'
+            );
+            return;
+        } else {
+            Notiflix.Notify.success(`Hooray! We found ${hitsLength} images.`);
 
+            renderImagesCards(hits);
+            // scrollImagesCards();
 
-function onload() {
-    
+            const lightbox = new SimpleLightbox('.gallery a', {
+                captionsData: 'alt',
+                captionDelay: 250,
+            });
+
+            lightbox.refresh();
+
+            loadMoreBtn.enable();
+            hitsLength += hits.length;
+        }
+    });
 }
+
+function renderImagesCards(images) {
+    const markup = images
+        .map(
+            image =>
+                `<div class="photo">
+  <a href="${image.largeImageURL}" class="gallery__item">
+    <img class="img" src="${image.webformatURL}" alt="${image.tags}" loading="lazy" />
+  </a>
+  <div class="info">
+    <p class="info-item"><b>Likes</b>${image.likes}</p>
+    <p class="info-item"><b>Views</b>${image.views}</p>
+    <p class="info-item"><b>Comments</b>${image.comments}</p>
+    <p class="info-item"><b>Downloads</b>${image.comments}</p>
+  </div>
+</div>`
+        )
+        .join('');
+
+    refs.galleryContainer.insertAdjacentHTML('beforeend', markup);
+}
+
+function clearGalleryContainer() {
+    refs.galleryContainer.innerHTML = '';
+}
+
+// function scrollImagesCards() {
+//     const { height: cardHeight } =
+//         refs.galleryContainer.firstElementChild.getBoundingClientRect();
+
+//     window.scrollBy({
+//         top: cardHeight * 2,
+//         behavior: 'smooth',
+//     });
+// }
+
+
--- src/service_image.js
+++ src/service_image.js
@@ -1,38 +1,43 @@
 import axios from 'axios';
 
-const API_KEY = '31748573-756ae01a8464a1c082026e61b';
-const BASE_URL = 'https://pixabay.com/api/';
+const API_KEY = '29727763-9de4927242ac493db1fc7e125';
+const BASE_URL = 'https://pixabay.com/api';
 
 
-export default class ImageQuery {
+export default class ImagesApiService {
     constructor() {
         this.searchQuery = '';
         this.page = 1;
     }
-    async fetchImage() {
-        
-        const url = `${BASE_URL}?key=${API_KEY}&q=${this.searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${this.page}`;
-        return await axios.get(url)
+
+    fetchImages() {
+        const url = `${BASE_URL}/?key=${API_KEY}&q=${this.searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&page=${this.page}&per_page=40`;
+        return axios
+            .get(url)
             .then(function (response) {
-                this.incrementPage();
+                console.log(response.data);
                 return response.data;
+            })
+            .then(({ totalHits, hits }) => {
+                this.incrementPage();
+                return { totalHits, hits };
             })
             .catch(error => console.log(error));
     }
 
-         incrementPage() {
-            this.page += 1;
-        }
+    incrementPage() {
+        this.page += 1;
+    }
 
-        resetPage() {
-            this.page = 1;
-        }
+    resetPage() {
+        this.page = 1;
+    }
 
-        get query() {
-            return this.searchQuery
-        }
+    get query() {
+        return this.searchQuery;
+    }
 
-        set query(newQuery){
-            this.searchQuery = newQuery;
-        }
+    set query(newQuery) {
+        this.searchQuery = newQuery;
+    }
 }
